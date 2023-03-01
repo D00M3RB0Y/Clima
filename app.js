86cf70150fb25dd9ac36d7f86277c43472c3b4c7
D00MERB0Y <107886313+D00MERB0Y@users.noreply.github.com>
2023-03-01 13:19:31 -0600

    Avance

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'e52c01534amshb66c4f51e499489p171cb7jsn4be06448604f',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'e52c01534amshb66c4f51e499489p171cb7jsn4be06448604f',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
 };
 
 fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+    .then(response => response.json())
+    .then((response) => {
+
+        console.log(response)
+        cloud_pct.innerHTML = response.cloud_pct
+        temp.innerHTML = response.temp
+        feels_like.innerHTML = response.feels_like
+        humidity.innerHTML = response.humidity
+        min_temp.innerHTML = response.min_temp
+        max_temp.innerHTML = response.max_temp
+        wind_speed.innerHTML = response.wind_speed
+        wind_degrees.innerHTML = response.wind_degrees
+        sunrise.innerHTML = response.sunrise
+        sunset.innerHTML = response.sunset
+    })
+    .catch(err => console.error(err)); 
